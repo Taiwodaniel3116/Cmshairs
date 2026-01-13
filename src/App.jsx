6d30c483677a4a50202b33865998a01bb6786615
TaiwoDaniel <taiwodaniel3116@gmail.com>
2026-01-12 17:13:54 -0800

    react boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Link } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='uppercase text-green-700 font-bold'>CMSHAIRS</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/data/product.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+   id: "1",
+  //  image: "/images/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+   image: "/images/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+   name: "Adult Cotton T-Shirt",
+   price: 200,
+   quantity: 1
+  },
+  {
+   id: "2",
+   image: "/images/backpack.jpg",
+   name: "Black Backpack",
+   price: 65,
+   quantity: 1
+  },
+]
