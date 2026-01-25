aea2bca1d912191022a4f0550447247b9ad49a76
TaiwoDaniel <taiwodaniel3116@gmail.com>
2026-01-24 17:00:19 -0800

    hero section update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,19 +19,19 @@ const Home = ({ totalQuantity, cartItems }) => {
       <div className="animate-fadeIn">
         {/* Hero section */}
 
-        <div className="pl-3 pt-17 h-max bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('/images/cms-hero.jpg')"}}>
+        <div className="pl-3 pt-20 h-max bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('/images/cms-hero.jpg')"}}>
           <span className="bg-green-900 text-white px-7 py-3 rounded-full text-xs font-semibold">
             New Collection 2026
           </span>
 
-          <h1 className="font-bold text-7xl/17 pt-10 pb-6 text-white ">
+          <h1 className="font-bold text-6xl/18 pt-10 pb-6 text-white ">
             Premium Quality Hair Extensions
           </h1>
           <p className="text-white text-[19px]">
             Transform your look with our luxurious collection of 100% human hair
             wigs and extensions. Ethically sourced, expertly crafted.
           </p>
-          <div className="mt-7 mb-2 w-fit h-max bg-green-900 shadow-2xs shadow-green-200 text-white px-10 py-3 rounded-full  transition duration-300 hover:bg-green-700 active:bg-green-700">
+          <div className="mt-7 mb-2 w-fit h-max bg-green-900 shadow-2xs shadow-green-200 text-white px-10 py-4 rounded-full  transition duration-300 hover:bg-green-700 active:bg-green-700">
             <Link to="/products">Shop Now →</Link>
           </div>
         </div>
